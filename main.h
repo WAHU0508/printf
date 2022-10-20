de592f00e22b96feb923de928f1efe5cc95234d7
WAHU0508 <sharongikenye@gmail.com>
2022-10-20 21:24:45 +0300

    Task 1

--- main.h
+++ main.h
@@ -17,6 +17,8 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int print_c(va_list c);
 int print_s(va_list);
+int print_d(va_list d);
+int print_i(va_list i);
 
 #endif  /* _MAIN_H */
 

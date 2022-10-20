7d17c10a0d9223bd64b70127d174e37dc189381a
WAHU0508 <sharongikenye@gmail.com>
2022-10-20 23:11:24 +0300

    Main.h update

--- main.h
+++ main.h
@@ -19,6 +19,9 @@ int print_c(va_list c);
 int print_s(va_list);
 int print_d(va_list d);
 int print_i(va_list i);
-
+int print_o(va_list o);
+int print_u(va_list u);
+int print_x(va_list x);
+int print_X(va_list X);
 #endif  /* _MAIN_H */
 
